6d762c186e0a5316ac4fd89e7d388bdbc6760018
Katerina <katerinaplatonova63@gmail.com>
2018-07-16 23:11:02 +0400

    Интерфейс

---
+++ WorkWithArrays.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public interface WorkWithArrays {
+
+    int getElement(int index);
+
+    void setElement(int value, int index);
+
+    void showArray();
+
+    void fillUpArray();
+
+    boolean deleteElement(int element);
+
+    void addElement(int element);
+
+    int findElement(int element);
+
+    void bubbleSort();
+
+    void selectSort();
+
+    void insertSort();
+
+}
